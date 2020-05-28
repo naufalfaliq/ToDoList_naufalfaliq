f2e99c003fa65610b17260e88d29bfd557c0595c
naufalfaliq <faliqmuhammadnaufal@gmail.com>
2020-05-28 20:47:28 +0800

    Model

---
+++ app/src/main/java/com/naufal/todolist/model/TodoModel.kt
@@ -0,0 +1,30 @@
+package com.naufal.todolist.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todotable")
+data class TodoModel(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "note")
+    var note: String? = null,
+
+    @ColumnInfo(name = "date_created")
+    var dateCreated: String,
+
+    @ColumnInfo(name = "date_updated")
+    var dateUpdated: String,
+
+    @ColumnInfo(name = "due_date")
+    var dueDate: String,
+
+    @ColumnInfo(name = "due_time")
+    var dueTime: String
+)
